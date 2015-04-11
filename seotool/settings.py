42b226c5ec289fc3d60698e097569f623f8ec688
monik <monik@bitbucket.org>
2015-04-11 09:44:34 +0530

    final changes

--- seotool/settings.py
+++ seotool/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wwg(1^u7*)z7u$t*@#3r=zm0g+x+vw%vm3l&tr^65ta=jwguib'
+SECRET_KEY = 'somekey'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
